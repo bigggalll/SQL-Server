5da78335b1a86bb95e007d992b4c36c173d1e9fc
Alain Martin <alainmartin00@gmail.com>
2023-03-31 13:34:56 -0400

    Update CPUUsageLast4H15M.sql

--- CPU/CPUUsageLast4H15M.sql
+++ CPU/CPUUsageLast4H15M.sql
@@ -1,4 +1,57 @@
- DECLARE @ts_now BIGINT
+
+-- utiliser cette version lorsque le # de cores est élevé (ax)
+DECLARE @ts_now BIGINT = (
+    SELECT
+        cpu_ticks / ( cpu_ticks / ms_ticks )
+    FROM
+        sys.dm_os_sys_info
+);
+with processor_Info_cte as (
+	select (cpu_count / hyperthread_ratio) as number_of_physical_cpus
+	from sys.dm_os_sys_info
+	),
+	utilization_cte as (
+		select top 1000 record_id
+			,sql_process_utilization
+			,system_idle
+			,100 - system_idle - sql_process_utilization as other_process_utilization
+			,[timestamp]
+		from (
+			select [timestamp],record.value('(./Record/@id)[1]', 'int') as record_id,
+				record.value('(./Record/SchedulerMonitorEvent/SystemHealth/SystemIdle)[1]', 'int') as system_idle,
+							record.value('(./Record/SchedulerMonitorEvent/SystemHealth/ProcessUtilization)[1]', 'int') as sql_process_utilization
+			from (
+				select convert(XML, record) as record,
+				[timestamp]
+				from sys.dm_os_ring_buffers
+				where ring_buffer_type = N'RING_BUFFER_SCHEDULER_MONITOR'
+						and record like '%<SystemHealth>%'
+				) as x
+		) as y order by record_id desc
+	)
+
+	select (case when other_process_utilization < 0 
+			then
+				sql_process_utilization/a.number_of_physical_cpus 
+			else
+				sql_process_utilization end) as sql_process_utilization
+		,system_idle/1.0 system_idle
+		,(case when other_process_utilization < 0 
+			then
+				100 - sql_process_utilization/a.number_of_physical_cpus -system_idle
+			else
+				other_process_utilization end) as other_process_utilization
+		,DATEADD(ms, -1 * ( @ts_now - [timestamp] )
+		,GETDATE()) AS [Event Time]
+	from utilization_cte cross apply (
+	select (number_of_physical_cpus*1.0) as number_of_physical_cpus from processor_Info_cte) as a
+
+
+
+
+
+-- vielle version
+DECLARE @ts_now BIGINT
 
 SELECT @ts_now = ms_ticks
 FROM sys.dm_os_sys_info
